58d2510cd254ac0857d39aeb550e52c4035df144
iamtheluiz <iamtheluiz.dev@gmail.com>
2022-12-09 23:45:29 -0300

    refactor: review code

--- esp32_codigo.ino
+++ esp32_codigo.ino
@@ -1,6 +1,6 @@
 #include <WiFi.h> //Inclui a biblioteca
-const char* ssid = "ElevenCar"; //Define o nome do ponto de acesso
-const char* pass = "cgqp6509"; //Define a senha
+const char* ssid = "lelindinha"; //Define o nome do ponto de acesso
+const char* pass = "mnbr6086"; //Define a senha
 
 
 //Confugurações dos pinos
@@ -9,10 +9,25 @@ int F1 = 13;
 int F2 = 12;
 int F3 = 14;
 int F4 = 27;
+int SetaFD = 32;
+int FarolFD = 33;
+int FreioFD = 22;
 int D1 = 5;
-int D2 = 18;
+int D2 = 4;
 int D3 = 19;
 int D4 = 21;
+int SetaFE = 26;  // Seta
+int FarolFE = 25;
+int FreioFE = 23;
+int Buzina = 18;
+
+// Estados:
+// - u => Frente
+// - r => Direita
+// - l => Esquerda
+// - d => Ré
+// - s => Parado
+char* state = "s"; // u, d, l, r, s
  
 WiFiServer sv(80); //Cria um servidor na porta 80
 void setup() {
@@ -20,22 +35,37 @@ void setup() {
   pinMode(F2, OUTPUT);
   pinMode(F3, OUTPUT);
   pinMode(F4, OUTPUT);
+  pinMode(SetaFD, OUTPUT);
+  pinMode(FarolFD, OUTPUT);
+  pinMode(FreioFD, OUTPUT);
 
   pinMode(D1, OUTPUT);
   pinMode(D2, OUTPUT);
   pinMode(D3, OUTPUT);
   pinMode(D4, OUTPUT);
+  pinMode(SetaFE, OUTPUT);
+  pinMode(FarolFE, OUTPUT);
+  pinMode(FreioFE, OUTPUT);
+
+  pinMode(Buzina, OUTPUT);
 
   //Deixar o carrinho com as rodas livres
   digitalWrite(F1, LOW);
   digitalWrite(F2, LOW);
   digitalWrite(F3, LOW);
   digitalWrite(F4, LOW);
+  digitalWrite(SetaFD, LOW);
+  digitalWrite(FarolFD, LOW);
+  digitalWrite(FreioFD, LOW);
 
   digitalWrite(D1, LOW);
   digitalWrite(D2, LOW);
   digitalWrite(D3, LOW);
-  digitalWrite(D4, LOW);
+  digitalWrite(SetaFE, LOW);
+  digitalWrite(FarolFE, LOW);
+  digitalWrite(FreioFE, LOW);
+
+  digitalWrite(Buzina, LOW);
 
   Serial.begin(115200); //Inicia o monitor serial
   pinMode(23, OUTPUT);  //Define a porta 23 como saída
@@ -53,6 +83,28 @@ void setup() {
   sv.begin(); //Inicia o servidor 
   Serial.println("Servidor online"); //Imprime a mensagem de início
 }
+
+void definirEstadoMotor(int pin1, int pin2, int estado) {
+  if (estado == 0) {  // Parado
+    digitalWrite(pin1, LOW);
+    digitalWrite(pin2, LOW);
+  } else if (estado == 1) { // Para frente
+    digitalWrite(pin1, HIGH);
+    digitalWrite(pin2, LOW);
+  } else if (estado == -1) {  // Para trás
+    digitalWrite(pin1, LOW);
+    digitalWrite(pin2, HIGH);
+  }
+}
+
+void acenderSeta(int setaPin) {
+  delay(300);
+  digitalWrite(setaPin, HIGH);
+  delay(300);
+  digitalWrite(setaPin, LOW);
+  
+}
+
 void loop() {
   WiFiClient client = sv.available(); //Cria o objeto cliente
   if (client) { //Se este objeto estiver disponível
@@ -71,73 +123,83 @@ void loop() {
         } else if (c != '\r') { 
           line += c; //Adiciona o caractere recebido à linha de leitura
         }
+        if (line.endsWith("GET /fon")) { //Se a linha terminar com "/ligar", liga o led
+          state = "s";
+          Serial.println("Buzina");
+          tone(Buzina, 440, 1000);
+        }
         if (line.endsWith("GET /up")) { //Se a linha terminar com "/ligar", liga o led
-          Serial.println("Tá indo para a frente");          
-          digitalWrite(F1, HIGH);
-          digitalWrite(F2, LOW);
-          digitalWrite(F3, HIGH);
-          digitalWrite(F4, LOW);
-
-          digitalWrite(D1, HIGH);
-          digitalWrite(D2, LOW);
-          digitalWrite(D3, HIGH);
-          digitalWrite(D4, LOW);
+          state = "u";
+          Serial.println("Tá indo para a frente");
+          definirEstadoMotor(F1, F2, 1);
+          definirEstadoMotor(F3, F4, 1);
+          definirEstadoMotor(D1, D2, 1);
+          definirEstadoMotor(D3, D4, 1);
           delay(100);
         }
         if (line.endsWith("GET /down")) { //Se a linha terminar com "/desligar", desliga o led
-          Serial.println("Tá indo para a Tras"); 
-          digitalWrite(F1, LOW);
-          digitalWrite(F2, HIGH);
-          digitalWrite(F3, LOW);
-          digitalWrite(F4, HIGH);
-
-          digitalWrite(D1, LOW);
-          digitalWrite(D2, HIGH);
-          digitalWrite(D3, LOW);
-          digitalWrite(D4, HIGH);   
+          state = "d";
+          Serial.println("Tá indo para a Tras");
+          definirEstadoMotor(F1, F2, -1);
+          definirEstadoMotor(F3, F4, -1);
+          definirEstadoMotor(D1, D2, -1);
+          definirEstadoMotor(D3, D4, -1);
           delay(100);       
         }
         if (line.endsWith("GET /left")) { //Se a linha terminar com "/ligar", liga o led
-          Serial.println("Tá indo para esquerda");   
-          digitalWrite(F1, HIGH);
-          digitalWrite(F2, LOW);
-          digitalWrite(F3, LOW);
-          digitalWrite(F4, HIGH);
-
-          digitalWrite(D1, HIGH);
-          digitalWrite(D2, LOW);
-          digitalWrite(D3, HIGH);
-          digitalWrite(D4, LOW);      
+          state = "l";
+          Serial.println("Tá indo para esquerda");
+          definirEstadoMotor(F1, F2, 1);
+          definirEstadoMotor(F3, F4, -1);
+          definirEstadoMotor(D1, D2, 1);
+          definirEstadoMotor(D3, D4, 1);
           delay(100);    
         }
         if (line.endsWith("GET /right")) { //Se a linha terminar com "/desligar", desliga o led
-          Serial.println("Tá indo para a direita");  
-          digitalWrite(F1, LOW);
-          digitalWrite(F2, HIGH);
-          digitalWrite(F3, HIGH);
-          digitalWrite(F4, LOW);
-
-          digitalWrite(D1, HIGH);
-          digitalWrite(D2, LOW);
-          digitalWrite(D3, HIGH);
-          digitalWrite(D4, LOW);     
+          state = "r";
+          Serial.println("Tá indo para a direita");
+          definirEstadoMotor(F1, F2, -1);
+          definirEstadoMotor(F3, F4, 1);
+          definirEstadoMotor(D1, D2, 1);
+          definirEstadoMotor(D3, D4, 1);
           delay(100);       
         }
         if (line.endsWith("GET /stop")) {
-          Serial.println("Está parado");  
-          digitalWrite(F1, LOW);
-          digitalWrite(F2, LOW);
-          digitalWrite(F3, LOW);
-          digitalWrite(F4, LOW);
-
-          digitalWrite(D1, LOW);
-          digitalWrite(D2, LOW);
-          digitalWrite(D3, LOW);
-          digitalWrite(D4, LOW);     
+          state = "s";
+          Serial.println("Está parado");
+          definirEstadoMotor(F1, F2, 0);
+          definirEstadoMotor(F3, F4, 0);
+          definirEstadoMotor(D1, D2, 0);
+          definirEstadoMotor(D3, D4, 0);
           delay(100);       
         }
       }
     }
     client.stop(); //Para o cliente
+  }
+
+  if (state == "r") { // Setas para a direita
+    acenderSeta(SetaFD);
+  }
+  if (state == "l") { // Setas para a esquerda
+    acenderSeta(SetaFE);
+  }
+  if (state == "u") { // Farol da frente
+    digitalWrite(FarolFD, HIGH);
+    digitalWrite(FarolFE, HIGH);
+  }
+  if (state == "d") { // Setas para a direita
+    digitalWrite(FreioFD, HIGH);
+    digitalWrite(FreioFE, HIGH);
+
+    tone(Buzina, 440, 500);
+    delay(800);
+    noTone(Buzina);
+  }
+  if (state == "s") { // Setas para a direita
+    digitalWrite(FarolFD, LOW);
+    digitalWrite(FarolFE, LOW);
+    digitalWrite(FreioFD, LOW);
+    digitalWrite(FreioFE, LOW);
   }
 }
